71379575b1f36c39f167cb98f1897d782b3367c5
DevRogerM <67657631+DevRogerM@users.noreply.github.com>
2022-11-12 10:41:15 -0500

    Update GameEnginerDriver.cpp
    updated the test driver for assignment2

--- GameEnginerDriver.cpp
+++ GameEnginerDriver.cpp
@@ -1,18 +1,63 @@
 #include "GameEngine.h";
+#include "CommandProcessing.h";
+
 #include <iostream>
 using namespace std;
 
-inline void testGameStates() {
-	StateController* sc = new StateController(new StartState);
-	while (true)
+inline void testCommandProcessor(int n) {
+
+	CommandProcessor* cp = new CommandProcessor();
+	FileCommandProcessorAdapter* fcp = new FileCommandProcessorAdapter();
+	StateController* sc = new StateController(new StartState());
+
+
+	//commands from console
+	if (n == 1) 
 	{
-		sc->UpdateState();
+		while (true)
+		{
+			sc->enterState(cp);							//greetings
+			sc->executeState(cp);							//call the commandProcessor methods until we get the right command, execute command 
+			sc->exitState(cp);							//transition to next state 	
+
+		}	
 	}
 
+	//commands from file
+
+	if (n == 2)
+	{
+		while (true)
+		{
+			sc->enterState(fcp);							//greetings
+			sc->executeState(fcp);							//call the commandProcessor methods until we get the right command, execute command 
+			sc->exitState(fcp);							//transition to next state 	
+		}
+	}
+
+
+	
+	int size = cp->commandCollection.size();
+
+	for (int i = 0; i < cp->commandCollection.size(); i++)
+	{
+		cout << "COMMAND: " << cp->commandCollection[i]->getCommand() << endl;
+		cout << "EFFECT : " << cp->commandCollection[i]->getEffect() << endl;
+		cout << endl;
+	}
+
+	int x = 5;
+	cin >> x;
+
+	
+
 }
 
 
-int main()
-{
-	testGameStates();
+int main() {
+
+	testCommandProcessor(1);
+
+
 }
+
